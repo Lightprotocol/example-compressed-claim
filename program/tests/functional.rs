80c3b28ceaeb27b2da811a38ec411720a3cd2120
Swenschaeferjohann <swen@lightprotocol.com>
2025-05-29 02:01:03 -0400

    wip

--- program/tests/functional.rs
+++ program/tests/functional.rs
@@ -1,5 +1,26 @@
 #![cfg(feature = "test-sbf")]
 
+use borsh::BorshSerialize;
+use light_compressed_account::{
+    address::derive_address, compressed_account::CompressedAccountWithMerkleContext,
+    hashv_to_bn254_field_size_be,
+};
+use light_program_test::{
+    program_test::LightProgramTest, AddressWithTree, Indexer, ProgramTestConfig, RpcConnection,
+    RpcError,
+};
+// use light_sdk::instruction::{
+//     account_meta::CompressedAccountMeta,
+//     accounts::SystemAccountMetaConfig,
+//     merkle_context::{pack_address_merkle_context, AddressMerkleContext},
+//     pack_accounts::PackedAccounts,
+// };
+
+use solana_sdk::{
+    instruction::Instruction,
+    pubkey::Pubkey,
+    signature::{Keypair, Signer},
+};
 use {
     light_compressed_account::compressed_account::PackedMerkleContext,
     light_compressed_claim::{
@@ -7,10 +28,8 @@ use {
         processor,
     },
     solana_program_test::*,
-    solana_sdk::{pubkey::Pubkey, signature::Signer},
 };
 
-// TODO: 1) add instruction builder (serialize, deserialize), 2) setup with test validator
 fn program_test() -> ProgramTest {
     ProgramTest::new(
         "light-compressed-claim",
@@ -21,12 +40,29 @@ fn program_test() -> ProgramTest {
 
 #[tokio::test]
 async fn test_claim_and_decompress() {
-    let context = program_test().start_with_context().await;
+    let config = ProgramTestConfig::new_v2(
+        true,
+        Some(vec![(
+            "light_compressed_claim",
+            light_compressed_claim::id(),
+        )]),
+    );
+    let mut rpc = LightProgramTest::new(config).await.unwrap();
+    let payer = rpc.get_payer().insecure_clone();
 
     // Setup test accounts
-    let claimant = context.payer.pubkey();
-    let fee_payer = context.payer.pubkey();
+    let claimant = payer.pubkey();
+    let fee_payer = payer.pubkey();
     let mint = Pubkey::new_unique();
+
+    // you need the validity proof lol! 
+    let compressed_pda = rpc
+    .indexer()
+    .unwrap()
+    .get_compressed_accounts_by_owner_v2(&light_compressed_claim::id())
+    .await
+    .unwrap()[0]
+    .clone();
 
     let accounts = ClaimAccounts {
         claimant: Pubkey::new_unique(),
